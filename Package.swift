72eb127dfb7b57963d4d159db17814987064672c
Oliver Atkinson <oatkinson@blockchain.com>
2022-12-06 15:42:17 +0000

    1.0.18

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileIntelligence",
-            url: "https://github.com/oliveratkinson-bc/swift-sardine/releases/download/v1.0.17/MobileIntelligence.xcframework.zip",
-            checksum: "9bf2f2e24c2f9bf7ed5169f754219b9735c97d89145d0c669afdb968b30005c3"
+            url: "https://github.com/oliveratkinson-bc/swift-sardine/releases/download/v1.0.18/MobileIntelligence.xcframework.zip",
+            checksum: "14b9610edb8399c5605365c4f1ec1bc4a988b90d5b79d07622edd644f8dfae7a"
         )
     ]
 )
